de9146c4f106143c8548ff8a7b9cdcb786c9fe3d
bwrosen20 <bwrosen20@gmail.com>
2023-01-22 14:22:03 -0500

    commit tree c5666e4f73019beaee9cfbbaaddef8bc3b0687c1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+
+const destinationURL= "http://localhost:3000/users";
+
+  
+function submitData(person,electricThing){
+    const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: person,
+        email: electricThing,}
+        ),
+  };
+  
+  fetch(destinationURL, configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function(object) {
+        let h2 = document.createElement('h2');
+        h2.innerHTML = object.id;
+        document.body.appendChild(h2);
+        console.log(object);
+    })
+    .catch(function(message) {
+        let h3 = document.createElement('h3');
+        h3.innerHTML = message;
+        document.body.appendChild(h3);
+        console.log(message);
+    });
+}
+
